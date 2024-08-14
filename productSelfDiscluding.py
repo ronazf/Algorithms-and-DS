85bdba17f654e34fe9d4f634f2d6518348f1a8d6
ronazfarahmand <ronazfarahmand1@gmail.com>
2024-08-14 15:47:09 -0400

    productDiscludingSelf

---
+++ productSelfDiscluding.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        res = [1] * length
+        product = 1
+        for i, num in enumerate(nums):
+            if i != length - 1:
+                product *= num
+                res[i + 1] = product
+      
+        product = 1
+        for i in range(length - 1, 0, -1):
+            product *= nums[i]
+            res[i - 1] = product * res[i - 1]
+      
+        return res
